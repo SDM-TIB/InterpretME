2696cfbd5bae0a668e5ce0e080f688b735a60b40
Philipp Rohde <Philipp.Rohde@tib.eu>
2022-05-05 12:09:50 +0200

    archive run in non-verbose mode

--- example/scripts/archive.sh
+++ example/scripts/archive.sh
@@ -4,7 +4,7 @@ cd /data/interpretme
 echo "Archiving run..."
 mkdir -p archive  # no error if archive already exists
 run_id=$( head -2 files/classes.csv  | tail -1 | cut -d ',' -f1 )
-GZIP=-9 tar cvzf ./archive/run_${run_id}.tar.gz ./files/ ./dataset/ ./output/
+GZIP=-9 tar czf ./archive/run_${run_id}.tar.gz ./files/ ./dataset/ ./output/
 rm -rf ./output ./files ./dataset
 echo "Done archiving!"
 
