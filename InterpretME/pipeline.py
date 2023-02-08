6e82b3df5f7cd5b66510243a443e028acb39b9a8
Yashraj chudasama <yashrajchudasama1995@gmail.com>
2023-02-08 13:58:51 +0100

    update pipeline for dataset input

--- InterpretME/pipeline.py
+++ InterpretME/pipeline.py
@@ -3,7 +3,7 @@ import json
 import os.path
 import time
 from pathlib import Path
-
+import csv
 import pandas as pd
 import validating_models.stats as stats
 from pkg_resources import resource_filename
@@ -13,9 +13,9 @@ from validating_models.dataset import BaseDataset, ProcessedDataset
 from validating_models.models.decision_tree import get_shadow_tree_from_checker
 from validating_models.shacl_validation_engine import ReducedTravshaclCommunicator
 
-from . import preprocessing_data, sampling_strategy, classification
-from .semantification import rdf_semantification
-from .upload import upload_to_virtuoso
+from InterpretME import preprocessing_data, sampling_strategy, classification
+from InterpretME.semantification import rdf_semantification
+from InterpretME.upload import upload_to_virtuoso
 
 
 def constraint_md5_sum(constraint):
@@ -40,8 +40,50 @@ def constraint_md5_sum(constraint):
     hash_md5.update(constraint.target_shape.encode(encoding='UTF-8', errors='ignore'))
     return hash_md5.hexdigest()
 
+def read_dataset(input_data,st):
+    path = input_data['path_to_data']
+    annotated_dataset = pd.read_csv(path)
+    #print(annotated_dataset)
+    seed_var = input_data['Index_var']
+    sampling = input_data['sampling_strategy']
+    cv = input_data['cross_validation_folds']
+    test_split = input_data['test_split']
+    num_imp_features = input_data['number_important_features']
+    train_model = input_data['model']
+    independent_var = []
+    dependent_var = []
+    classes = []
+    class_names = []
+    definition = []
+        # Create the dataset
+    for k, v in enumerate(input_data['Independent_variable']):
+        independent_var.append(v)
+        print("data_list:",independent_var)
+        definition.append(v)
+    for k, v in enumerate(input_data['Dependent_variable']):
+        dependent_var.append(v)
+        print("data_list:", dependent_var)
+        definition.append(v)
 
-def read(input_file, st):
+    features = independent_var + dependent_var
+
+    for k, v in input_data['classes'].items():
+        classes.append(v)
+        class_names.append(k)
+
+    with stats.measure_time('PIPE_OUTPUT'):
+        df1 = pd.DataFrame({'features': pd.Series(features), 'definition': pd.Series(definition)})
+        df1.loc[:, 'run_id'] = st
+        df1 = df1.set_index('run_id')
+        df1.to_csv('interpretme/files/feature_definition.csv')
+
+        df2 = pd.DataFrame({'classes': pd.Series(classes)})
+        df2.loc[:, 'run_id'] = st
+        df2 = df2.set_index('run_id')
+        df2.to_csv('interpretme/files/classes.csv')
+    return seed_var, independent_var, dependent_var, classes, class_names, st, sampling, test_split, num_imp_features, train_model, cv, annotated_dataset
+
+def read_KG(input_data, st):
     """Reads from the input knowledge graphs and extracts data.
 
     Extracting features and its definition from the input knowledge graphs using sparqlQuery
@@ -61,40 +103,37 @@ def read(input_file, st):
         Annotated dataset with SHACL results and class information given by the user.
 
     """
-    with open(input_file, "r") as input_file_descriptor:
-        input_data = json.load(input_file_descriptor)
+    endpoint = input_data['Endpoint']
+    independent_var = []
+    dependent_var = []
+    classes = []
+    class_names = []
+    definition = []
 
-        endpoint = input_data['Endpoint']
-        seed_var = input_data['Index_var']
-        sampling = input_data['sampling_strategy']
-        cv = input_data['cross_validation_folds']
-        test_split = input_data['test_split']
-        num_imp_features = input_data['number_important_features']
-        train_model = input_data['model']
+    seed_var = input_data['Index_var']
+    sampling = input_data['sampling_strategy']
+    cv = input_data['cross_validation_folds']
+    test_split = input_data['test_split']
+    num_imp_features = input_data['number_important_features']
+    train_model = input_data['model']
 
-        independent_var = []
-        dependent_var = []
-        classes = []
-        class_names = []
-        definition = []
+    # Create the dataset generating query
+    query_select_clause = "SELECT "
+    query_where_clause = """WHERE { """
+    for k, v in input_data['Independent_variable'].items():
+        independent_var.append(k)
+        # print("data_list:",independent_var)
+        query_select_clause = query_select_clause + "?" + k + " "
+        query_where_clause = query_where_clause + v
+        definition.append(v)
 
-        # Create the dataset generating query
-        query_select_clause = "SELECT "
-        query_where_clause = """WHERE { """
-        for k, v in input_data['Independent_variable'].items():
-            independent_var.append(k)
-            # print("data_list:",independent_var)
-            query_select_clause = query_select_clause + "?" + k + " "
-            query_where_clause = query_where_clause + v
-            definition.append(v)
-
-        for k, v in input_data['Dependent_variable'].items():
-            dependent_var.append(k)
-            # print("target_list:", dependent_var)
-            query_select_clause = query_select_clause + "?" + k + " "
-            query_where_clause = query_where_clause + v
-            target_name = k
-            definition.append(v)
+    for k, v in input_data['Dependent_variable'].items():
+        dependent_var.append(k)
+        # print("target_list:", dependent_var)
+        query_select_clause = query_select_clause + "?" + k + " "
+        query_where_clause = query_where_clause + v
+        target_name = k
+        definition.append(v)
 
         query_where_clause = query_where_clause + "}"
         sparqlQuery = query_select_clause + " " + query_where_clause
@@ -103,89 +142,90 @@ def read(input_file, st):
         features = independent_var + dependent_var
 
         shacl_engine_communicator = ReducedTravshaclCommunicator(
-            '', endpoint, resource_filename('InterpretME', 'shacl_api_config.json')
+        '', endpoint, resource_filename('InterpretME', 'shacl_api_config.json')
+           )
+
+    def hook(results):
+        bindings = [{key: value['value'] for key, value in binding.items()}
+                    for binding in results['results']['bindings']]
+        df = pd.DataFrame.from_dict(bindings)
+        for column in df.columns:
+            df[column] = df[column].str.rsplit('/', 1).str[-1]
+        return df
+
+    with stats.measure_time('PIPE_DATASET_EXTRACTION'):
+        base_dataset = BaseDataset.from_knowledge_graph(endpoint, shacl_engine_communicator, sparqlQuery,
+                                                    target_name, seed_var=seed_var,
+                                                    raw_data_query_results_to_df_hook=hook)
+
+    constraints = [ShaclSchemaConstraint.from_dict(constraint) for constraint in input_data['Constraints']]
+    constraint_identifiers = [constraint_md5_sum(constraint) for constraint in constraints]
+
+    with stats.measure_time('PIPE_SHACL_VALIDATION'):
+        shacl_validation_results = base_dataset.get_shacl_schema_validation_results(
+                constraints, rename_columns=True, replace_non_applicable_nans=True
         )
 
-        def hook(results):
-            bindings = [{key: value['value'] for key, value in binding.items()}
-                        for binding in results['results']['bindings']]
-            df = pd.DataFrame.from_dict(bindings)
-            for column in df.columns:
-                df[column] = df[column].str.rsplit('/', 1).str[-1]
-            return df
+    sample_to_node_mapping = base_dataset.get_sample_to_node_mapping().rename('node')
 
-        with stats.measure_time('PIPE_DATASET_EXTRACTION'):
-            base_dataset = BaseDataset.from_knowledge_graph(endpoint, shacl_engine_communicator, sparqlQuery,
-                                                            target_name, seed_var=seed_var,
-                                                            raw_data_query_results_to_df_hook=hook)
+    annotated_dataset = pd.concat(
+            (base_dataset.df, shacl_validation_results, sample_to_node_mapping), axis='columns'
+    )
 
-        constraints = [ShaclSchemaConstraint.from_dict(constraint) for constraint in input_data['Constraints']]
-        constraint_identifiers = [constraint_md5_sum(constraint) for constraint in constraints]
+    annotated_dataset = annotated_dataset.drop_duplicates()
+    annotated_dataset = annotated_dataset.set_index(seed_var)
 
-        with stats.measure_time('PIPE_SHACL_VALIDATION'):
-            shacl_validation_results = base_dataset.get_shacl_schema_validation_results(
-                constraints, rename_columns=True, replace_non_applicable_nans=True
+    for k, v in input_data['classes'].items():
+        classes.append(v)
+        class_names.append(k)
+
+    with stats.measure_time('PIPE_OUTPUT'):
+        df1 = pd.DataFrame({'features': pd.Series(features), 'definition': pd.Series(definition)})
+        df1.loc[:, 'run_id'] = st
+        df1 = df1.set_index('run_id')
+        df1.to_csv('interpretme/files/feature_definition.csv')
+
+        df2 = pd.DataFrame({'classes': pd.Series(classes)})
+        df2.loc[:, 'run_id'] = st
+        df2 = df2.set_index('run_id')
+        df2.to_csv('interpretme/files/classes.csv')
+
+        dfs_shacl_results = []
+
+        for constraint, identifier in zip(constraints, constraint_identifiers):
+            df6 = pd.DataFrame(annotated_dataset.loc[:, [constraint.name]]).rename(
+                columns={constraint.name: 'SHACL result'})
+            df6['run_id'] = st
+            df6['SHACL schema'] = constraint.shape_schema_dir
+            df6['SHACL shape'] = constraint.target_shape.rsplit('/', 1)[1][:-1]  # remove the prefix from the shape name
+            df6['SHACL constraint name'] = constraint.name
+            df6['constraint identifier'] = identifier
+
+            df6 = df6.reset_index()
+            df6 = df6.rename(columns={df6.columns[0]: 'index'})
+            dfs_shacl_results.append(df6)
+            pd.concat(dfs_shacl_results, axis='rows').to_csv(
+                'interpretme/files/shacl_validation_results.csv', index=False
             )
 
-        sample_to_node_mapping = base_dataset.get_sample_to_node_mapping().rename('node')
+        df7 = pd.DataFrame(annotated_dataset.loc[:, ['node']])
+        df7['run_id'] = st
+        df7 = df7.drop_duplicates()
+        df7 = df7.reset_index()
+        df7 = df7.rename(columns={df7.columns[0]: 'index'})
+        df7.to_csv('interpretme/files/entityAlignment.csv', index=False)
 
-        annotated_dataset = pd.concat(
-            (base_dataset.df, shacl_validation_results, sample_to_node_mapping), axis='columns'
-        )
-
-        annotated_dataset = annotated_dataset.drop_duplicates()
-        annotated_dataset = annotated_dataset.set_index(seed_var)
-
-        for k, v in input_data['classes'].items():
-            classes.append(v)
-            class_names.append(k)
-
-        with stats.measure_time('PIPE_OUTPUT'):
-            df1 = pd.DataFrame({'features': pd.Series(features), 'definition': pd.Series(definition)})
-            df1.loc[:, 'run_id'] = st
-            df1 = df1.set_index('run_id')
-            df1.to_csv('interpretme/files/feature_definition.csv')
-
-            df2 = pd.DataFrame({'classes': pd.Series(classes)})
-            df2.loc[:, 'run_id'] = st
-            df2 = df2.set_index('run_id')
-            df2.to_csv('interpretme/files/classes.csv')
-
-            dfs_shacl_results = []
-
-            for constraint, identifier in zip(constraints, constraint_identifiers):
-                df6 = pd.DataFrame(annotated_dataset.loc[:, [constraint.name]]).rename(
-                    columns={constraint.name: 'SHACL result'})
-                df6['run_id'] = st
-                df6['SHACL schema'] = constraint.shape_schema_dir
-                df6['SHACL shape'] = constraint.target_shape.rsplit('/', 1)[1][:-1]  # remove the prefix from the shape name
-                df6['SHACL constraint name'] = constraint.name
-                df6['constraint identifier'] = identifier
-
-                df6 = df6.reset_index()
-                df6 = df6.rename(columns={df6.columns[0]: 'index'})
-                dfs_shacl_results.append(df6)
-                pd.concat(dfs_shacl_results, axis='rows').to_csv(
-                    'interpretme/files/shacl_validation_results.csv', index=False
-                )
-
-            df7 = pd.DataFrame(annotated_dataset.loc[:, ['node']])
-            df7['run_id'] = st
-            df7 = df7.drop_duplicates()
-            df7 = df7.reset_index()
-            df7 = df7.rename(columns={df7.columns[0]: 'index'})
-            df7.to_csv('interpretme/files/entityAlignment.csv', index=False)
-
-            df8 = pd.DataFrame({'endpoint': pd.Series(endpoint)})
-            df8.loc[:, 'run_id'] = st
-            df8 = df8.set_index('run_id')
-            df8.to_csv('interpretme/files/endpoint.csv')
+        df8 = pd.DataFrame({'endpoint': pd.Series(endpoint)})
+        df8.loc[:, 'run_id'] = st
+        df8 = df8.set_index('run_id')
+        df8.to_csv('interpretme/files/endpoint.csv')
 
     annotated_dataset = annotated_dataset.drop(columns=['node'])
     num = len(input_data['Constraints'])
     annotated_dataset = annotated_dataset.iloc[:, :-num]
 
     return seed_var, independent_var, dependent_var, classes, class_names, annotated_dataset, constraints, base_dataset, st, input_data['3_valued_logic'],sampling, test_split, num_imp_features, train_model, cv
+
 
 
 # get the start time and use it as run_id
@@ -243,7 +283,25 @@ def pipeline(path_config, lime_results, server_url, username, password,
     stats.STATS_COLLECTOR.activate(hyperparameters=[])
     stats.STATS_COLLECTOR.new_run(hyperparameters=[])
 
-    seed_var, independent_var, dependent_var, classes, class_names, annotated_dataset, constraints, base_dataset, st, non_applicable_counts, samplingstrategy, train_test_split, num_imp_features, train_model, cross_validation = read(path_config, st)
+    with open(path_config, "r") as input_file_descriptor:
+        input_data = json.load(input_file_descriptor)
+
+    input_is_kg = None
+    if "Endpoint" in input_data.keys() and "path_to_data" not in input_data.keys():
+        # input from SPARQL endpoint
+        input_is_kg = True
+        seed_var, independent_var, dependent_var, classes, class_names, annotated_dataset, constraints, base_dataset, st, non_applicable_counts, path, samplingstrategy, train_test_split, num_imp_features, train_model, cross_validation = read_KG(
+            path_config, st)
+
+    elif "Endpoint" not in input_data.keys() and "path_to_data" in input_data.keys():
+        # input from dataset
+        input_is_kg = False
+        seed_var, independent_var, dependent_var, classes, class_names, st, sampling, test_split, num_imp_features, train_model, cv, annotated_dataset = read_dataset(input_data,st)
+
+    else:
+        # error
+        raise Exception ("Please provide either SPARQL endpoint or Dataset")
+
 
     if sampling is None:
         sampling = samplingstrategy
@@ -295,25 +353,26 @@ def pipeline(path_config, lime_results, server_url, username, password,
     processed_df = pd.concat((new_sampled_data, sampled_target), axis='columns')
     processed_df.reset_index(inplace=True)
 
-    with stats.measure_time('PIPE_CONSTRAINT_VIZ'):
-        processedDataset = ProcessedDataset.from_node_unique_columns(
-            processed_df,
-            base_dataset,
-            base_columns=[seed_var],
-            target_name='class',
-            categorical_mapping={'class': {i: classes[i] for i in range(len(classes))}}
-        )
-        checker = Checker(clf.predict, processedDataset)
+    if "Endpoint" in input_data.keys() and "path_to_data" not in input_data.keys():
+        with stats.measure_time('PIPE_CONSTRAINT_VIZ'):
+            processedDataset = ProcessedDataset.from_node_unique_columns(
+                processed_df,
+                base_dataset,
+                base_columns=[seed_var],
+                target_name='class',
+                categorical_mapping={'class': {i: classes[i] for i in range(len(classes))}}
+            )
+            checker = Checker(clf.predict, processedDataset)
 
-        shadow_tree = get_shadow_tree_from_checker(clf, checker)
+            shadow_tree = get_shadow_tree_from_checker(clf, checker)
 
-        results['checker'] = checker
-        results['shadow_tree'] = shadow_tree
-        results['constraints'] = constraints
-        results['non_applicable_counts'] = non_applicable_counts
+            results['checker'] = checker
+            results['shadow_tree'] = shadow_tree
+            results['constraints'] = constraints
+            results['non_applicable_counts'] = non_applicable_counts
 
     with stats.measure_time('PIPE_InterpretMEKG_SEMANTIFICATION'):
-        rdf_semantification()
+        rdf_semantification(input_is_kg)
 
     with stats.measure_time('PIPE_InterpretMEKG_UPLOAD_VIRTUOSO'):
         upload_to_virtuoso(run_id=st, rdf_file='./rdf-dump/interpretme.nt',
@@ -331,4 +390,4 @@ def pipeline(path_config, lime_results, server_url, username, password,
 
 
 if __name__ == '__main__':
-    pipeline('../example/example_french_royalty.json', './LIME', 'http://localhost:8891/sparql', 'dba', 'dba')
+    pipeline('../example/example_drugs_dataset.json', './LIME', 'http://localhost:8891/sparql', 'dba', 'dba')
--- InterpretME/semantification.py
+++ InterpretME/semantification.py
@@ -6,15 +6,20 @@ semantification = stats.get_decorator('PIPE_InterpretMEKG_SEMANTIFICATION')
 
 
 @semantification
-def rdf_semantification():
+def rdf_semantification(input_is_kg: bool):
     print("#####################################################################")
     print("******* Semantifying traced metadata from predictive pipeline *******")
     print("#####################################################################")
     main_path = resource_filename('InterpretME', '')
+    if input_is_kg:
+        number_of_datasets = 12
+    else:
+        number_of_datasets = 8
+
     config = '[default]\n' \
              'main_directory: ' + main_path + '\n\n' \
                                               '[datasets]\n' \
-                                              'number_of_datasets: 11\n' \
+                                              'number_of_datasets: ' + str(number_of_datasets) + '\n' \
                                               'output_folder: ./rdf-dump\n' \
                                               'all_in_one_file: yes\n' \
                                               'remove_duplicate: yes\n' \
@@ -30,35 +35,37 @@ def rdf_semantification():
                                               'name: cross_validation\n' \
                                               'mapping: ${default:main_directory}/mappings/cross_validation.ttl\n\n' \
                                               '[dataset3]\n' \
-                                              'name: endpoint\n' \
-                                              'mapping: ${default:main_directory}/mappings/endpoint.ttl\n\n' \
-                                              '[dataset4]\n' \
-                                              'name: feature_definition\n' \
-                                              'mapping: ${default:main_directory}/mappings/definition_feature.ttl\n\n' \
-                                              '[dataset5]\n' \
                                               'name: imp_features\n' \
                                               'mapping: ${default:main_directory}/mappings/imp_features.ttl\n\n' \
-                                              '[dataset6]\n' \
+                                              '[dataset4]\n' \
                                               'name: model_details\n' \
                                               'mapping: ${default:main_directory}/mappings/model_details.ttl\n\n' \
-                                              '[dataset7]\n' \
+                                              '[dataset5]\n' \
                                               'name: precision_recall\n' \
                                               'mapping: ${default:main_directory}/mappings/precision_recall.ttl\n\n' \
-                                              '[dataset8]\n' \
+                                              '[dataset6]\n' \
                                               'name: sampling_strategy\n' \
                                               'mapping: ${default:main_directory}/mappings/sampling_strategy.ttl\n\n' \
-                                              '[dataset9]\n' \
-                                              'name: shacl_validation_results\n' \
-                                              'mapping: ${default:main_directory}/mappings/shacl_validation_results.ttl\n\n' \
-                                              '[dataset10]\n' \
+                                              '[dataset7]\n' \
                                               'name: lime_features\n' \
                                               'mapping: ${default:main_directory}/mappings/lime_features.ttl\n\n' \
-                                              '[dataset11]\n' \
+                                              '[dataset8]\n' \
                                               'name: prediction_probabilities\n' \
                                               'mapping: ${default:main_directory}/mappings/prediction_probabilities.ttl\n\n'
-
+    if input_is_kg:
+        config += '[dataset9]\n' \
+                  'name: endpoint\n' \
+                  'mapping: ${default:main_directory}/mappings/endpoint.ttl\n\n' \
+                  '[dataset10]\n' \
+                  'name: feature_definition\n' \
+                  'mapping: ${default:main_directory}/mappings/definition_feature.ttl\n\n' \
+                  '[dataset11]\n' \
+                  'name: shacl_validation_results\n' \
+                  'mapping: ${default:main_directory}/mappings/shacl_validation_results.ttl\n\n' \
+                  '[dataset12]\n' \
+                  'name: entity_alignment\n' \
+                  'mapping: ${default:main_directory}/mappings/entity_alignment.ttl\n\n'
     with open('config_rdfizer.ini', 'w', encoding='utf8') as config_file:
         config_file.write(config)
 
-    read_mappings = semantify("config_rdfizer.ini")
-    print(read_mappings)
+    semantify("config_rdfizer.ini")
