3fba2e685e2c6c4f14535a7d61fa1602bccc72e4
Philipp Rohde <Philipp.Rohde@tib.eu>
2023-09-25 13:34:03 +0200

    pass config dict to SDM-RDFizer
    require newer version of SDM-RDFizer

--- InterpretME/semantification.py
+++ InterpretME/semantification.py
@@ -9,63 +9,71 @@ semantification = stats.get_decorator('PIPE_InterpretMEKG_SEMANTIFICATION')
 
 @semantification
 def rdf_semantification(input_is_kg: bool):
-    main_path = resource_filename('InterpretME', '')
-    if input_is_kg:
-        number_of_datasets = 12
-    else:
-        number_of_datasets = 8
+    config = {
+        'default': {
+            'main_directory': resource_filename('InterpretME', '')
+        },
+        'datasets': {
+            'number_of_datasets': '12' if input_is_kg else '8',
+            'output_folder': './rdf-dump',
+            'all_in_one_file': 'yes',
+            'remove_duplicate': 'yes',
+            'name': 'interpretme',
+            'enrichment': 'yes',
+            'ordered': 'yes',
+            'large_file': 'false'
+        },
+        'dataset1': {
+            'name': 'classes',
+            'mapping': '${default:main_directory}/mappings/classes.ttl'
+        },
+        'dataset2': {
+            'name': 'cross_validation',
+            'mapping': '${default:main_directory}/mappings/cross_validation.ttl'
+        },
+        'dataset3': {
+            'name': 'imp_features',
+            'mapping': '${default:main_directory}/mappings/imp_features.ttl'
+        },
+        'dataset4': {
+            'name': 'model_details',
+            'mapping': '${default:main_directory}/mappings/model_details.ttl'
+        },
+        'dataset5': {
+            'name': 'precision_recall',
+            'mapping': '${default:main_directory}/mappings/precision_recall.ttl'
+        },
+        'dataset6': {
+            'name': 'sampling_strategy',
+            'mapping': '${default:main_directory}/mappings/sampling_strategy.ttl'
+        },
+        'dataset7': {
+            'name': 'lime_features',
+            'mapping': '${default:main_directory}/mappings/lime_features.ttl'
+        },
+        'dataset8': {
+            'name': 'prediction_probabilities',
+            'mapping': '${default:main_directory}/mappings/prediction_probabilities.ttl'
+        }
+    }
 
-    config = '[default]\n' \
-             'main_directory: ' + main_path + '\n\n' \
-                                              '[datasets]\n' \
-                                              'number_of_datasets: ' + str(number_of_datasets) + '\n' \
-                                              'output_folder: ./rdf-dump\n' \
-                                              'all_in_one_file: yes\n' \
-                                              'remove_duplicate: yes\n' \
-                                              'name: interpretme\n' \
-                                              'enrichment: yes\n' \
-                                              'dbtype: mysql\n' \
-                                              'ordered: yes\n' \
-                                              'large_file: false\n\n' \
-                                              '[dataset1]\n' \
-                                              'name: classes\n' \
-                                              'mapping: ${default:main_directory}/mappings/classes.ttl\n\n' \
-                                              '[dataset2]\n' \
-                                              'name: cross_validation\n' \
-                                              'mapping: ${default:main_directory}/mappings/cross_validation.ttl\n\n' \
-                                              '[dataset3]\n' \
-                                              'name: imp_features\n' \
-                                              'mapping: ${default:main_directory}/mappings/imp_features.ttl\n\n' \
-                                              '[dataset4]\n' \
-                                              'name: model_details\n' \
-                                              'mapping: ${default:main_directory}/mappings/model_details.ttl\n\n' \
-                                              '[dataset5]\n' \
-                                              'name: precision_recall\n' \
-                                              'mapping: ${default:main_directory}/mappings/precision_recall.ttl\n\n' \
-                                              '[dataset6]\n' \
-                                              'name: sampling_strategy\n' \
-                                              'mapping: ${default:main_directory}/mappings/sampling_strategy.ttl\n\n' \
-                                              '[dataset7]\n' \
-                                              'name: lime_features\n' \
-                                              'mapping: ${default:main_directory}/mappings/lime_features.ttl\n\n' \
-                                              '[dataset8]\n' \
-                                              'name: prediction_probabilities\n' \
-                                              'mapping: ${default:main_directory}/mappings/prediction_probabilities.ttl\n\n'
     if input_is_kg:
-        config += '[dataset9]\n' \
-                  'name: endpoint\n' \
-                  'mapping: ${default:main_directory}/mappings/endpoint.ttl\n\n' \
-                  '[dataset10]\n' \
-                  'name: feature_definition\n' \
-                  'mapping: ${default:main_directory}/mappings/definition_feature.ttl\n\n' \
-                  '[dataset11]\n' \
-                  'name: shacl_validation_results\n' \
-                  'mapping: ${default:main_directory}/mappings/shacl_validation_results.ttl\n\n' \
-                  '[dataset12]\n' \
-                  'name: entity_alignment\n' \
-                  'mapping: ${default:main_directory}/mappings/entity_alignment.ttl\n\n'
-    with open('config_rdfizer.ini', 'w', encoding='utf8') as config_file:
-        config_file.write(config)
+        config['dataset9'] = {
+            'name': 'endpoint',
+            'mapping': '${default:main_directory}/mappings/endpoint.ttl'
+        }
+        config['dataset10'] = {
+            'name': 'feature_definition',
+            'mapping': '${default:main_directory}/mappings/definition_feature.ttl'
+        }
+        config['dataset11'] = {
+            'name': 'shacl_validation_results',
+            'mapping': '${default:main_directory}/mappings/shacl_validation_results.ttl'
+        }
+        config['dataset12'] = {
+            'name': 'entity_alignment',
+            'mapping': '${default:main_directory}/mappings/entity_alignment.ttl'
+        }
 
     with HiddenPrints():
-        semantify("config_rdfizer.ini")
+        semantify(config)
